aac7469b5627381638ddaa583f67c75a6a66a02d
jun yan  wu <bayoh807@gmail.com>
2024-11-07 19:32:11 +0800

    fix : api report

--- app/api/report/route.ts
+++ app/api/report/route.ts
@@ -5,17 +5,17 @@ export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const queryString = searchParams.toString(); // 直接生成查詢字串
 
-    // const response = await fetch(`${process.env.API_URL}/report?${queryString}`, {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    // });
-    //
-    //
-    // const data = await response.json();
+    const response = await fetch(`${process.env.API_URL}/report?${queryString}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 
-    return new Response(JSON.stringify(example), {
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
         status: 200,
         headers: {
             "Content-Type": "application/json",
